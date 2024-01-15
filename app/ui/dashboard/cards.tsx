5537c5cdd5a3eb05f82447638e8217d27b00ee04
j19015 <j19015@sangi.jp>
2024-01-16 04:39:38 +0900

    fix 練習問題(Cardコンポーネント)

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -15,17 +15,22 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  const cardData= await fetchCardData();
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      <Card title="Collected" value={cardData.totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={cardData.totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={cardData.numberOfInvoices} type="invoices" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
-        value={cardData.numberOfCustomers}
+        value={numberOfCustomers}
         type="customers"
       />
     </>
