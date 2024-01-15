60b1352b128a14a5beef9c313f296b8a967ab7fb
Koki Takahashi <koki-takahashi@vcube.co.jp>
2024-01-16 04:39:38 +0900

    fix 練習問題(Cardコンポーネント)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,19 +7,25 @@ import { fetchRevenue, fetchLatestInvoices,fetchCardData } from '@/app/lib/data'
 export default async function Page() {
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
-  const cardData= await fetchCardData();
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={cardData.totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={cardData.totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={cardData.numberOfInvoices} type="invoices" />
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
         <Card
           title="Total Customers"
-          value={cardData.numberOfCustomers}
+          value={numberOfCustomers}
           type="customers"
         />
       </div>
